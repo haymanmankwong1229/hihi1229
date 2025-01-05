9dfb704fafd0a7b0b1f60c5f2ed5413de9838e44
haymanmankwong1229 <haymankwong@gmail.com>
2025-01-05 14:01:42 +0800

    new

---
+++ hello.js
@@ -0,0 +1,7 @@
+let firstname = 'Hayman'
+let lastname = 'Kwong'
+let fullname = firstname + " " + lastname
+let age = "xx"
+let sentence1 = "I am" 
+let sentence2 = "My age is"
+console.log(sentence1 + " " + fullname + "." + sentence2 + " " + age)
