52ecd58b391cb26892cb6ca623e75eb0b52c8c03
haymanmankwong1229 <haymankwong@gmail.com>
2025-01-05 14:07:48 +0800

    new1

--- hello.js
+++ hello.js
@@ -4,4 +4,5 @@ let fullname = firstname + " " + lastname
 let age = "xx"
 let sentence1 = "I am" 
 let sentence2 = "My age is"
-console.log(sentence1 + " " + fullname + "." + sentence2 + " " + age)
+console.log(sentence1 + " " + fullname + "." + sentence2 + " " + age + ".")
+
